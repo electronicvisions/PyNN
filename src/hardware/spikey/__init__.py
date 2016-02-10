2ba7493bd345579c8682c5321f03c535c1192942
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2016-02-10 18:57:31 +0100

    Fix crash when setting parameters with value zero
    When calling tset/set with a zero parameter, the program crashes with
    an exception, e.g.
    
        pop.tset("tau_refrac", [0.0, 0.0, 0.0, 0.0])
    
    crashes with
    
        Exception: ERROR: Cell 0 has no parameter t!
    
    This patch fixes this problem by explicitly checking whether
    the value is "None" (the default parameter value) and not
    blindly converting the value to a boolean expression.
    
    Change-Id: Ib6629f298b3c716e4ccf336504af3b4941a15199

--- src/hardware/spikey/__init__.py
+++ src/hardware/spikey/__init__.py
@@ -1049,7 +1049,7 @@ def set(cells, cellclass, param, val=None):
     global _neuronsChanged
     global _inputChanged
 
-    if val:
+    if val is not None:
         param = {param: val}
     if type(cells) != types.ListType:
         cells = [cells]
